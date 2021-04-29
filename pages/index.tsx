5720737b1606ccd3ee3c9ae5a38a86f6def05a0f
kajirikajiri <kajirikajiri@gmail.com>
2021-04-29 21:49:35 +0900

    :sparkles: (support amp)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+export const config = {
+  amp: true,
+};
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
