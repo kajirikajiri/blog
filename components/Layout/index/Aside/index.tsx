9d59013292ee298177065352679d0d881e8b24f9
kajirikajiri <kajirikajiri@gmail.com>
2021-05-09 16:41:56 +0900

    :sparkles: (algolia) add

--- components/Layout/index/Aside/index.tsx
+++ components/Layout/index/Aside/index.tsx
@@ -2,6 +2,7 @@ import { TreemapData } from "@/types/treemapData";
 import { Box } from "@material-ui/core";
 import { Category } from "../../../Category";
 import { Profile } from "./index/Profile";
+import { Search } from "./index/Search";
 
 type Props = {
   treemapData: TreemapData;
@@ -13,13 +14,14 @@ export const Aside = ({ treemapData }: Props) => {
       {/* <Box height={300} bgcolor="pink" marginBottom={"42px"}>
         広告
       </Box> */}
-      {/* <Box height={45} bgcolor="pink" marginBottom={"45px"}>
-        検索
-      </Box> */}
       <Profile />
       <Box height={20}> </Box>
       <Box width="100%" height="600px">
         <Category treemapData={treemapData} />
+      </Box>
+      <Box height={20}> </Box>
+      <Box height={45} marginBottom={"45px"}>
+        <Search />
       </Box>
       {/* <Box height={10} bgcolor="pink" marginBottom={"45px"}>
         TOC
