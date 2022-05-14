1ea4218886bb543ed3f8f724a303fa6e2df214f1
kajirikajiri <kajirikajiri@gmail.com>
2022-05-15 00:22:47 +0900

    Make the code readable

--- components/Layout/index/Aside/index.tsx
+++ components/Layout/index/Aside/index.tsx
@@ -20,26 +20,48 @@ export const Aside = () => {
       {pathNames[0] !== "profile" && <Profile />}
       <Box height={80}> </Box>
       {pathNames[0] !== "category" && (
-        <Box
-          width="100%"
-          borderColor="#000"
-          border="1px solid"
-          color="#000"
-          height={40}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          position="relative"
-        >
-          <a
-            href={"/category"}
-            color="inherit"
-            style={{ position: "absolute", width: "100%", height: "100%" }}
-          ></a>
-          カテゴリ一覧
-          <ChevronRight fontSize="small" />
-        </Box>
+        <>
+          <Box
+            width="100%"
+            borderColor="#000"
+            border="1px solid"
+            color="#000"
+            height={40}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            position="relative"
+          >
+            <a
+              href={"/category"}
+              color="inherit"
+              style={{ position: "absolute", width: "100%", height: "100%" }}
+            ></a>
+            カテゴリ一覧
+            <ChevronRight fontSize="small" />
+          </Box>
+          <Box marginTop={2} />
+        </>
       )}
+      <Box
+        width="100%"
+        borderColor="#000"
+        border="1px solid"
+        color="#000"
+        height={40}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        position="relative"
+      >
+        <a
+          href={"/scrawl"}
+          color="inherit"
+          style={{ position: "absolute", width: "100%", height: "100%" }}
+        ></a>
+        はしりがき
+        <ChevronRight fontSize="small" />
+      </Box>
       {/* <Box height={10} bgcolor="pink" marginBottom={"45px"}>
         TOC
       </Box> */}
