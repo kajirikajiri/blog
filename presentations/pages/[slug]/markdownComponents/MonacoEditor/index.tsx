8a3b276173d02d22e812e5f153cc8eacbdef46b2
kajirikajiri <kajirikajiri@gmail.com>
2023-04-16 19:53:05 +0900

    Update message passing logic

--- presentations/pages/[slug]/markdownComponents/MonacoEditor/index.tsx
+++ presentations/pages/[slug]/markdownComponents/MonacoEditor/index.tsx
@@ -8,14 +8,51 @@ export const MonacoEditor = ({ code }: Props) => {
   const ref = useRef<HTMLIFrameElement>(null);
   const origin = "https://monaco-editor.pages.dev";
   useEffect(() => {
-    console.log(code, origin);
-    const iframe = ref.current;
-    console.log(iframe);
-    if (!iframe) return;
-    const iframeWindow = iframe.contentWindow;
-    console.log(iframeWindow);
-    if (!iframeWindow) return;
-    iframeWindow.postMessage(code, origin);
+    (async () => {
+      const messageListener = new Promise<{ success: boolean }>((resolve) => {
+        setTimeout(() => {
+          window.removeEventListener("message", callback);
+          resolve({
+            success: false,
+          });
+        }, 1000);
+        const callback = (
+          event: MessageEvent<Partial<{ success: boolean }>>
+        ) => {
+          if (event.origin !== origin) return;
+          if (!event.data?.success) return;
+          window.removeEventListener("message", callback);
+          resolve({
+            success: true,
+          });
+        };
+        window.addEventListener("message", callback);
+      });
+      for await (const _ of Array.from({ length: 5 })) {
+        const iframeWindow = ref.current?.contentWindow;
+        if (!iframeWindow) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          continue;
+        }
+
+        const result = await Promise.all([
+          messageListener,
+          iframeWindow.postMessage(code, origin),
+        ])
+          .then((value) => {
+            if (value[0].success) {
+              return true;
+            }
+          })
+          .catch((e) => {
+            console.error(e);
+          });
+        console.log(result);
+        if (result) {
+          break;
+        }
+      }
+    })();
   }, []);
   return (
     <iframe
