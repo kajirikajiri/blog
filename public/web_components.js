97f6faca5b934787d4a7e7c8915d1e1e219260a9
kajirikajiri <kajirikajiri@gmail.com>
2024-03-24 14:30:15 +0900

    FOUC fix

--- public/web_components.js
+++ public/web_components.js
@@ -1,31 +1,21 @@
 //https://stackoverflow.com/questions/70201172/this-getattribute-not-working-in-web-component-while-violating-the-spec
 
-customElements.define('my-link', class extends HTMLElement {
-  connectedCallback() {
-    // https://stackoverflow.com/a/53813523
-    setTimeout(() => {
-      const text = this.innerHTML
-      this.innerHTML = ""
-
-      const a = document.createElement('a')
-      const div = document.createElement('div')
-      this.appendChild(a)
-      a.appendChild(div)
-
-      a.href = this.getAttribute('href')
-
-      div.innerHTML = text
-      div.style.margin = "8px 0px"
-      div.style.padding = "8px 0px"
-    })
-  }
-});
-
-customElements.define('hint-box', class extends HTMLElement {
+customElements.define('blog-name', class extends HTMLElement {
   constructor() {
     super();
     const template = document.getElementById(
-      "hint-box-template",
+      "blog-name-template",
+    ).content;
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.cloneNode(true));
+  }
+});
+
+customElements.define('blog-author', class extends HTMLElement {
+  constructor() {
+    super();
+    const template = document.getElementById(
+      "blog-author-template",
     ).content;
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(template.cloneNode(true));
@@ -54,22 +44,32 @@ customElements.define('post-link-content', class extends HTMLElement {
   }
 });
 
-customElements.define('blog-name', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "blog-name-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
+customElements.define('my-link', class extends HTMLElement {
+  connectedCallback() {
+    // https://stackoverflow.com/a/53813523
+    setTimeout(() => {
+      const text = this.innerHTML
+      this.innerHTML = ""
+
+      const a = document.createElement('a')
+      const div = document.createElement('div')
+      this.appendChild(a)
+      a.appendChild(div)
+
+      a.href = this.getAttribute('href')
+
+      div.innerHTML = text
+      div.style.margin = "8px 0px"
+      div.style.padding = "8px 0px"
+    })
   }
 });
 
-customElements.define('blog-author', class extends HTMLElement {
+customElements.define('hint-box', class extends HTMLElement {
   constructor() {
     super();
     const template = document.getElementById(
-      "blog-author-template",
+      "hint-box-template",
     ).content;
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.appendChild(template.cloneNode(true));
