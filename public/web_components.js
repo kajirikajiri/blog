41d62c77087b756d1b463faed2be1080c956c409
kajirikajiri <kajirikajiri@gmail.com>
2024-03-24 21:00:16 +0900

    WebComponent.js update

--- public/web_components.js
+++ public/web_components.js
@@ -1,47 +1,14 @@
 //https://stackoverflow.com/questions/70201172/this-getattribute-not-working-in-web-component-while-violating-the-spec
 
-customElements.define('blog-name', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "blog-name-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
-  }
-});
-
-customElements.define('blog-author', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "blog-author-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
-  }
-});
-
-customElements.define('post-link', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "post-link-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
-  }
-});
-
-customElements.define('post-link-content', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "post-link-content-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
-  }
+['blog-name', 'blog-author', 'post-link', 'post-link-content', 'hint-box'].forEach(tag => {
+  customElements.define(tag, class extends HTMLElement {
+    constructor() {
+      super();
+      const template = document.getElementById(tag+"-template").content;
+      const shadowRoot = this.attachShadow({ mode: "open" });
+      shadowRoot.appendChild(template.cloneNode(true));
+    }
+  });
 });
 
 customElements.define('my-link', class extends HTMLElement {
@@ -62,17 +29,6 @@ customElements.define('my-link', class extends HTMLElement {
       div.style.margin = "8px 0px"
       div.style.padding = "8px 0px"
     })
-  }
-});
-
-customElements.define('hint-box', class extends HTMLElement {
-  constructor() {
-    super();
-    const template = document.getElementById(
-      "hint-box-template",
-    ).content;
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.appendChild(template.cloneNode(true));
   }
 });
 
